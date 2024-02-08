1969b89efe32a828eaaf3775db71d1262e31b263
Ethan Enger <engerethan@gmail.com>
2024-02-07 21:44:35 -0700

    Initial

---
+++ QrCodeLab/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QrCodeLab
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ QrCodeLab/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
+
+namespace QrCodeLab
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.UseBarcodeReader()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
